2e057c33b1e35a77e92241bd9acf61589ee68af3
MagicOgre <nagyrobert@bsmt1.eu>
2024-07-16 23:06:01 +0200

    Update qbittorrent_debian.sh

--- install_scripts/qbittorrent_debian.sh
+++ install_scripts/qbittorrent_debian.sh
@@ -1,1 +1,28 @@
-asd
+#!/bin/bash
+
+apt update
+apt upgrade
+
+apt install qbittorrent-nox
+
+adduser --system --group qbittorrent-nox
+adduser qbittorrent-nox qbittorrent-nox
+
+echo "[Unit]
+Description=qBittorrent Command Line Client
+After=network.target
+
+[Service]
+Type=forking
+User=qbittorrent-nox
+Group=qbittorrent-nox
+UMask=007
+ExecStart=/usr/bin/qbittorrent-nox -d --webui-port=8080
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/qbittorrent-nox.service
+
+systemctl daemon-reload
+systemctl start qbittorrent-nox
+systemctl enable qbittorrent-nox
